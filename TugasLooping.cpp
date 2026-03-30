b8ad3774c6c0f274f9d9f3c421ee5aa7dab6c94f
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-03-30 23:35:41 +0700

    menambahkan kondisi pembagi pada isPrima()

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -10,5 +10,8 @@ bool isPrima(int x) {
     if (x <= 1) return false;
 
     int i = 2;
-    while (i <= x / 2)
+    while (i <= x / 2) {
+        if (x % i == 0) {
+            return false;
+        }
 }
